a894d00176c14a83a0dcf3677b96376687bba534
kh7j <kh7j@fresnou.org>
2022-10-13 11:31:32 -0700

    commit thursday

--- routes/index.js
+++ routes/index.js
@@ -18,8 +18,31 @@ router.post('/card', function(req, res){
     city: req.body.city,
     state: req.body.state,
     zip: req.body.zip,
-    accountNumber: 12345,
-    currentDate: new Date()
+    accountNumber: createAccountNumber(),
+    currentDate: new Date(),
+    cardClass: getCardClass(req.body.type)
   });
 });
+
 module.exports = router;
+
+function createAccountNumber(){
+  let accNum = ""
+  for (let i = 0; i<5; i++){
+    let temp = Math.floor(Math.random()*10);
+    accNum += temp;
+  }
+  return accNum;
+}
+
+function getCardClass(type){
+  if (type === "Premium"){
+    return "premium"
+  } else if (type === "Standard"){
+    return "standard"
+  } else if (type === "Bronze"){
+    return "bronze"
+  } else {
+    return ""
+  }
+}
